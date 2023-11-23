951de5ba308dce1f366a37ecda2ec1bd72dc2cc7
Daniel Lindgren <daniel.lindgren.88.so@gmail.com>
2023-11-23 08:36:39 +0100

    First commit. Base program

---
+++ src/main/java/org/example/LoginWindow.java
@@ -0,0 +1,55 @@
+package org.example;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
+public class LoginWindow {
+    private JButton loginButton;
+    private JTextField passwordField;
+    private JTextField usernameField;
+    private JPanel Window1;
+
+    private ArrayList<UserList> users = new ArrayList<>();
+
+    public LoginWindow() {
+        JFrame jFrame = new JFrame("Login Window");
+        jFrame.setVisible(true);
+        jFrame.setSize(300, 150);
+        jFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        jFrame.setContentPane(Window1);
+        jFrame.setLocationRelativeTo(null);
+
+        // Lägger till användare i listan
+        users.add(new UserList("Lars", "123"));
+        users.add(new UserList("Arta", "234"));
+        users.add(new UserList("Daniel", "345"));
+        users.add(new UserList("Mickey", "456"));
+        users.add(new UserList("Anders", "567"));
+
+        loginButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String username = usernameField.getText();
+                String password = passwordField.getText();
+                if (authenticate(username, password)) {
+                    new MainMenu();
+                } else {
+                        JOptionPane.showMessageDialog(jFrame,"Wrong password or usename, Please try again!");
+                        usernameField.setText("");
+                        passwordField.setText("");
+                }
+            }
+        });
+    }
+    private boolean authenticate(String username, String password) {
+        for (UserList user : users) {
+            if (user.getUsername().equals(username) && user.getPassword().equals(password)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
---
+++ src/main/java/org/example/MainMenu.java
@@ -0,0 +1,22 @@
+package org.example;
+
+import javax.swing.*;
+
+public class MainMenu {
+    private JButton paymentButton;
+    private JButton accountButton;
+    private JButton adminButton;
+    private JButton transactionButton;
+    private JPanel Window2;
+    private JLabel Mainmenu;
+
+    public MainMenu() {
+        JFrame jFrame = new JFrame("Main menu");
+        jFrame.setVisible(true);
+        jFrame.setSize(450, 300);
+        jFrame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+        jFrame.setContentPane(Window2);
+        jFrame.setLocationRelativeTo(null);
+
+    }
+}
