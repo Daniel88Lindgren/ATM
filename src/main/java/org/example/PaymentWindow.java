c18c8ac6f19f373f566caab18cfe05b6cb526bb8
Arta100 <artarrustemi85@hotmail.com>
2023-11-24 14:13:28 +0100

    Payment

---
+++ src/main/java/org/example/PaymentWindow.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import javax.swing.*;
+
+public class PaymentWindow {
+    private JPanel panel1;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+}
+// hoppas det funkar nu.
